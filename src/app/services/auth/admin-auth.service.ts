92ae8a584e86735e1ee565d5551d04ee36b106c8
RohitSth <shrestha.rohit655@gmail.com>
2025-01-12 20:26:43 +0545

    update : made actions and service for admin sigin

--- src/app/services/auth/admin-auth.service.ts
+++ src/app/services/auth/admin-auth.service.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { createAdminSchema } from "@/app/schema/admin";
+import { createAdminSchema, signInAdminSchema } from "@/app/schema/admin";
 import { endpoints } from "@/core/contants/endpoints";
 import { AuthErrorResponse } from "@/core/types/auth.interface";
 import { z } from "zod";
@@ -25,4 +25,26 @@ export const createAdminService = async (
   }
 
   return newAdmin;
+};
+
+export const signInAdminService = async (
+  data: z.infer<typeof signInAdminSchema>
+) => {
+  const res = await fetch(endpoints.auth.admin.signin, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  const admin: AuthErrorResponse = await res.json();
+
+  if (!res.ok) {
+    const error = new Error(admin.message);
+    (error as any).fieldErrors = admin.message;
+    throw error;
+  }
+
+  return admin;
 };

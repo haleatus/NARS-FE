4fc489a9c67eba0c1f6c87b4513a56ebf698b54c
RohitSth <shrestha.rohit655@gmail.com>
2025-01-23 20:06:52 +0545

    update : minor update in admin users page

--- src/app/(admin)/admin/users/page.tsx
+++ src/app/(admin)/admin/users/page.tsx
@@ -1,52 +1,12 @@
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { Button } from "@/components/ui/button";
+import GetAllUsersServer from "@/app/_components/users/get-all-users-server";
+import React from "react";
 
-export default function Users() {
-  const users = [
-    { id: 1, name: "Alice Cooper", email: "alice@example.com", role: "User" },
-    { id: 2, name: "Bob Dylan", email: "bob@example.com", role: "User" },
-    {
-      id: 3,
-      name: "Charlie Brown",
-      email: "charlie@example.com",
-      role: "User",
-    },
-  ];
-
+const Users = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold mb-6">User Management</h1>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Name</TableHead>
-            <TableHead>Email</TableHead>
-            <TableHead>Role</TableHead>
-            <TableHead>Action</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {users.map((user) => (
-            <TableRow key={user.id}>
-              <TableCell>{user.name}</TableCell>
-              <TableCell>{user.email}</TableCell>
-              <TableCell>{user.role}</TableCell>
-              <TableCell>
-                <Button variant="outline" size="sm">
-                  Edit
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <GetAllUsersServer />
     </div>
   );
-}
+};
+
+export default Users;

b47af731ff95c39e9548e6ebcd08aea532ca7d8e
RohitSth <shrestha.rohit655@gmail.com>
2025-02-05 13:16:59 +0545

    fix : fixed the infinite loop issue

--- src/components/home/landing-page.client.tsx
+++ src/components/home/landing-page.client.tsx
@@ -5,7 +5,7 @@ import { useAdmin } from "@/context/admin-context";
 import { useAmbulance } from "@/context/ambulance-context";
 import { useUser } from "@/context/user-context";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function HomeClient() {
   const { user } = useUser();
@@ -14,19 +14,18 @@ export default function HomeClient() {
 
   const [displayName, setDisplayName] = useState<string>("Guest");
 
-  if (user) {
-    setDisplayName(user.fullname);
-  } else if (ambulance) {
-    setDisplayName(ambulance.driver_name);
-  } else if (admin) {
-    setDisplayName(admin.username);
-  }
+  useEffect(() => {
+    if (user) {
+      setDisplayName(user.fullname);
+    } else if (ambulance) {
+      setDisplayName(ambulance.driver_name);
+    } else if (admin) {
+      setDisplayName(admin.username);
+    }
+  }, [user, ambulance, admin]); // Only runs when any of these values change
 
   return (
     <div className="container mx-auto p-4 space-y-8 font-sans scroll-smooth [scrollbar-width:none]">
-      {/* <div className="fixed bottom-4 right-8 flex justify-center items-center flex-col z-50">
-        <EmergencyButton />
-      </div> */}
       {/* Main Content */}
       <main className="container mx-auto px-4 mt-8 grid grid-cols-1 lg:grid-cols-2 gap-4">
         <div className="space-y-6">
